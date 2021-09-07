07cf33f9a2cd58461b6fc4f0bff253eac8431ce8
Oluwasegun Adesanya <oluwasegunadesanya@Oluwaseguns-MacBook-Pro.local>
2021-09-07 18:13:25 +0200

    second commit

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,17 +1,59 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
+import { any } from 'bluebird';
 
 (async () => {
+
+
+    // Importing express-csp-header
+  const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
 
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
+
+  // Use of Middleware Wrapper for Csp Header
+  app.use(expressCspHeader({
+    directives: {
+      'default-src': [SELF],
+      'script-src': [SELF, INLINE],
+      'img-src': ['data:', 'images.com'],
+      'worker-src': [NONE],
+      'block-all-mixed-content': true
+    }
+  }));
+
+
+  app.get("/filteredimage", async (req, res) => {
+
+    let filterimageUrl: string = req.query.filterimageUrl;
+
+    let acceptedImageUrl: any = filterimageUrl.match("http://localhost:{{PORT}}/filteredimage?image_url=https://upload.wikimedia.org/wikipedia/commons/b/bd/Golden_tabby_and_white_kitten_n01.jpg");
+    
+    let filteredImage: string = await filterImageFromURL(filterimageUrl);
+
+    if (acceptedImageUrl == false)
+      return res.status(401).send("Enter a valid url");
+
+    else {
+
+      if (filteredImage === null || filteredImage === undefined)
+        return res.status(401).send("image cannot be filtered try again!");
+
+      else {
+        res.on("Completed", function () {
+          deleteLocalFiles([filteredImage]);
+        });
+        return res.status(200).sendFile("" + filteredImage);
+      }
+    }
+  })
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
@@ -30,17 +72,16 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{URL}}")
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`My server is running now at http://localhost:${port}`);
+    console.log(`To stop the server, press CTRL+C to stop server`);
+  });
 })();
